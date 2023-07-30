b6b70c2ff0717b98a3a425d8e3cecb3769400a60
Andrey V <Andreyxvv@hotmail.com>
2023-07-30 11:59:47 +0500

    second commit

--- HomeWorkCPP6.3.2.cpp
+++ HomeWorkCPP6.3.2.cpp
@@ -10,27 +10,34 @@ void exponentiation(T& elem) {
     elem *= elem;
 }
 
-template<typename It, typename T>  
-void my_for_each(It begin, It end, void(*function)(T&), int flow = 8) {
+template<typename It, typename T>
+void my_for_each(It begin, It end, void(*function)(T&)) {
 
-    if (flow > 1) {
+    for (auto iter = begin; iter != end; iter++) {
 
-        auto size = std::distance(begin, end);
+        function(*iter);
+    }
+}
+
+template<typename It, typename T, size_t size_flow = 100>  
+void my_parallel_for_each(It begin, It end, void(*function)(T&)) {
+
+    auto size = std::distance(begin, end);
+
+    if (size > size_flow) {
+
         auto middle = begin;
         std::advance(middle, size / 2);
 
-        auto fl1 = std::async(my_for_each<It, T>, begin, middle, function, flow / 2);
-        auto fl2 = std::async(my_for_each<It, T>, middle, end, function, flow / 2);
+        auto fl1 = std::async(my_parallel_for_each<It, T>, begin, middle, function);
+        my_parallel_for_each(middle, end, function);
     }
     else {
 
-        for (auto iter = begin; iter != end; iter++) {
-
-            function(*iter);
-        }
+        my_for_each(begin, end, function);
     }
 
-    //std::cout << std::this_thread::get_id() << std::endl;
+    std::cout << std::this_thread::get_id() << std::endl;
 }
 
 void vector_random(std::vector<int>& num, int size_vector) {
@@ -57,12 +64,12 @@ void print(std::vector<int>& num) {
 
 int main() {
 
-    int size = 16000;
+    int size = 1600;
     std::vector<int>vec;
 
     vector_random(vec, size);
     //print(vec);
 
-    my_for_each(vec.begin(), vec.end(), exponentiation<int>);
+    my_parallel_for_each(vec.begin(), vec.end(), exponentiation<int>);
     //print(vec);
 }
